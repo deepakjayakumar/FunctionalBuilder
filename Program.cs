76f99a51ee441796ab16bf4818c228c684297372
deepakjayakumar <deepakjayakumar11@gmail.com>
2023-08-08 21:12:08 +0530

    personbuilder

--- Program.cs
+++ Program.cs
@@ -1,5 +1,35 @@
 ﻿namespace FunctionalBuilder
 {
+
+    public class Person
+    {
+        public string Name, Position;
+    }
+
+    public sealed class PersonBuilder{
+
+        private readonly List<Func<Person,Person>> actions = new List<Func<Person,Person>>();
+
+        public PersonBuilder Called(string name)
+        {
+            Do(p => p.Name = name);
+            return this;
+        }
+        public PersonBuilder Do(Action<Person> action)
+        {
+            AddAction(action);
+            return this;
+        }
+        private PersonBuilder AddAction(Action<Person> action) {
+            actions.Add(p =>
+            {
+                action(p); return p;
+            });
+
+            return this;
+        }
+
+        }
     internal class Program
     {
         static void Main(string[] args)
