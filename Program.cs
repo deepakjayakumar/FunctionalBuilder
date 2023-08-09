cac6c2222a98fe8fe0da07e199cb9896950d5c8f
deepakjayakumar <deepakjayakumar11@gmail.com>
2023-08-09 17:45:33 +0530

    Implemented functional builder

--- Program.cs
+++ Program.cs
@@ -1,49 +1,102 @@
-﻿namespace FunctionalBuilder
+﻿using System.Runtime.CompilerServices;
+
+namespace FunctionalBuilder
 {
 
     public class Person
     {
         public string Name, Position;
+
+        public override string? ToString()
+        {
+            return $"{nameof(Name)} : {Name} , {nameof(Position)} : {Position}";
+        }
     }
 
-    public sealed class PersonBuilder{
+    public abstract class FunctionBuilder<TSubject,Self>
+        where Self : FunctionBuilder<TSubject,Self>
+        where TSubject :new()
 
-        private readonly List<Func<Person,Person>> actions = new List<Func<Person,Person>>();
+    {
+        private readonly List<Func<TSubject, TSubject>> actions = new List<Func<TSubject, TSubject>>();
 
-        public PersonBuilder Called(string name)
-        {
-            Do(p => p.Name = name);
-            return this;
-        }
-        public PersonBuilder Do(Action<Person> action)
+        public Self Do(Action<TSubject> action)
         {
             AddAction(action);
-            return this;
+            return (Self) this;
         }
 
-        public PersonBuilder Build()
+        public Self Build()
         {
-            actions.Aggregate(new Person(),(p,f) => f(p));
-            return this;
+            actions.Aggregate(new TSubject(), (p, f) => f(p));
+            return (Self) this;
         }
-        private PersonBuilder AddAction(Action<Person> action) {
+        private Self AddAction(Action<TSubject> action)
+        {
             actions.Add(p =>
             {
                 action(p); return p;
             });
 
+            return (Self) this;
+        }
+
+    }
+
+
+    public sealed class PersonBuilder : FunctionBuilder<Person,PersonBuilder>
+    {
+        public PersonBuilder Called(string name)
+        {
+            Do(p => p.Name = name);
             return this;
         }
 
-        }
 
-    
+    }
+    //public sealed class PersonBuilder{
+
+    //    private readonly List<Func<Person,Person>> actions = new List<Func<Person,Person>>();
+
+    //    public PersonBuilder Called(string name)
+    //    {
+    //        Do(p => p.Name = name);
+    //        return this;
+    //    }
+    //    public PersonBuilder Do(Action<Person> action)
+    //    {
+    //        AddAction(action);
+    //        return this;
+    //    }
+
+    //    public PersonBuilder Build()
+    //    {
+    //        actions.Aggregate(new Person(),(p,f) => f(p));
+    //        return this;
+    //    }
+    //    private PersonBuilder AddAction(Action<Person> action) {
+    //        actions.Add(p =>
+    //        {
+    //            action(p); return p;
+    //        });
+
+    //        return this;
+    //    }
+
+    //    }
+
+    public static class PersonBuilderExtension
+    {
+        public static PersonBuilder WorksAs
+            (this PersonBuilder builder, string Position)
+            => builder.Do(p => p.Position = Position);
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            var person = new PersonBuilder().Called("a").Build();
-            Console.WriteLine("Hello, World!");
+            var person = new PersonBuilder().Called("a").WorksAs("painter").Build();
+            Console.WriteLine(person.ToString());
         }
     }
 }
